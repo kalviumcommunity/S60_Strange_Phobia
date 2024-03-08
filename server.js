31b76f48c608b26d188fee3ff4f7fa526a90c30f
KarthikaMovva <karthika.movva@kalvium.community>
2024-03-08 12:53:00 +0530

    added route

--- server.js
+++ server.js
@@ -3,8 +3,14 @@
 const exp = require("express")
 const useExp = exp()
 
-useExp.listen(3000,()=>{console.log("3000")})
-
-useExp.get("/ping",(req,res)=>{
+useExp.get("/",(req,res)=>{
+    res.send("basic message")
+})
+useExp.get("/",(req,res)=>{
     res.send("message")
 })
+useExp.use(exp.static('public'));
+
+useExp.listen(3000,()=>{console.log("3000")});
+
+
